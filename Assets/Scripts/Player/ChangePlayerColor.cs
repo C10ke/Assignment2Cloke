93914658794954cdf7e016a139974e846485e9ee
C10ke <78232488+C10ke@users.noreply.github.com>
2021-02-13 19:34:31 -0600

    finished assignment 2

---
+++ Assets/Scripts/Player/ChangePlayerColor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePlayerColor : MonoBehaviour
+{
+    public Player player;
+    public Dropdown myDropDown;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(myDropDown.value)
+        {
+            case 0:
+                player.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case 1:
+                player.GetComponent<Renderer>().material.color = Color.blue;
+                break;
+            case 2:
+                player.GetComponent<Renderer>().material.color = Color.green;
+                break;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/SpeedController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedController : MonoBehaviour
+{
+
+    public Slider mySlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySlider.onValueChanged.AddListener(delegate{ValueChangeCheck(); });
+    }
+    public void ValueChangeCheck()
+    {
+        Debug.Log(mySlider.value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
